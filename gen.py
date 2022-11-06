29fac391fe2b8e5683d73269c7faec8537f19c1e
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2022-11-06 05:06:43 -0600

    .

--- gen.py
+++ gen.py
@@ -1,20 +1,19 @@
 from data import hm
 
 print('Please enter job description.')
-jd = input()
+jd = input().split()
 
-jd_vec = jd.split()
 ar = ["For the skills listed in the jd, "
       "I've aced the corresponding LinkedIn Skills Assessments."
-      "You may verify this on my profile. \n"]
+      "You may verify this on my profile. \n\n"]
 
-for word in jd_vec:
+for word in jd:
     if word in hm:
         ar.append(word)
         ar.append(': Top ')
         ar.append(hm[word][0])
         ar.append('\n')
-        ar.append(' ')
+        ar.append('    ')
         ar.append(hm[word][1])
         ar.append('\n')
 
