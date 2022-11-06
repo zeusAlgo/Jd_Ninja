952261392db8b50e285ede08cb65354441024f64
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2022-11-06 06:55:02 -0600

    .

--- gen.py
+++ gen.py
@@ -3,15 +3,17 @@ from data import hm
 
 print('Please enter job description.')
 
-jd_ar = []
+s = ''
 for line in sys.stdin:
-    if 'exit' == line.rstrip(): break
-    jd_ar.extend(line.split())
+    if 'e' == line.rstrip(): break
+    s += line
 
 ar = ["For the skills listed in the jd, I've aced the corresponding LinkedIn Skills Assessments."
       "You may verify this on my profile. \n\n"]
 
-for word in jd_ar:
+vector = s.split()
+
+for word in vector:
     if word in hm:
         ar.append(word)
         ar.append(': Top ')
