a75ed6fc26ac22611f403d3d2df14ba9dba388f1
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2022-11-06 18:35:55 -0600

    .

--- gen.py
+++ gen.py
@@ -8,7 +8,7 @@ for line in sys.stdin:
     if 'e' == line.rstrip(): break
     s += line
 
-to_remove_hs = {':', '!', ',', '(', ')', '{', '}', ';', '+', '-'}
+to_remove_hs = {':', '!', ',', '(', ')', '{', '}', ';', '+', '-', '#', '$', '%', '&', '*', '@', '?'}
 s = ''.join(c for c in s if c not in to_remove_hs)
 
 ar = ["For the skills listed in the job description, I've aced the corresponding LinkedIn Skills Assessments. "
@@ -25,7 +25,7 @@ for word in words_hs:
         ar.append('    ')
         ar.append(hm[word][1])
         ar.append('\n')
-print(hs)
+
 ar.append(" \nMy app Job Description Ninja created the above report. "
           "For you it's FOSS - Free and Open Source Software. "
           "Check it out on my Github https://github.com/zeusAlgo/Jd_Ninja")
