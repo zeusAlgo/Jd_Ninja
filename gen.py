1730d220aa4f1fee5e1f1fab8e9ae69cd045fb23
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2022-11-06 18:38:54 -0600

    .

--- gen.py
+++ gen.py
@@ -11,7 +11,7 @@ for line in sys.stdin:
 to_remove_hs = {':', '!', ',', '(', ')', '{', '}', ';', '+', '-', '#', '$', '%', '&', '*', '@', '?'}
 s = ''.join(c for c in s if c not in to_remove_hs)
 
-ar = ["For the skills listed in the job description, I've aced the corresponding LinkedIn Skills Assessments. "
+ar = ["\nFor the skills listed in the job description, I've aced the corresponding LinkedIn Skills Assessments. "
       "You may verify this on my profile. \n\n"]
 
 words_hs = set(s.split())
@@ -28,5 +28,5 @@ for word in words_hs:
 
 ar.append(" \nMy app Job Description Ninja created the above report. "
           "For you it's FOSS - Free and Open Source Software. "
-          "Check it out on my Github https://github.com/zeusAlgo/Jd_Ninja")
+          "Available here -> https://github.com/zeusAlgo/Jd_Ninja")
 print(''.join(ar))
