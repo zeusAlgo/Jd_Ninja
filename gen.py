c559c87b5263e3db49054db92bd7e90359fb3459
zeusAlgo <36017479+zeusAlgo@users.noreply.github.com>
2022-11-06 18:27:25 -0600

    .

--- gen.py
+++ gen.py
@@ -8,6 +8,13 @@ for line in sys.stdin:
     if 'e' == line.rstrip(): break
     s += line
 
+s = s.replace(':', '')
+s = s.replace('!', '')
+s = s.replace(',', '')
+s = s.replace(')', '')
+s = s.replace('(', '')
+s = s.replace(';', '')
+
 ar = ["For the skills listed in the job description, I've aced the corresponding LinkedIn Skills Assessments. "
       "You may verify this on my profile. \n\n"]
 
@@ -22,7 +29,7 @@ for word in hs:
         ar.append('    ')
         ar.append(hm[word][1])
         ar.append('\n')
-
+print(hs)
 ar.append(" \nMy app Job Description Ninja created the above report. "
           "For you it's FOSS - Free and Open Source Software. "
           "Check it out on my Github https://github.com/zeusAlgo/Jd_Ninja")
